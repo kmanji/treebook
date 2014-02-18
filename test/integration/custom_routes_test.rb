ae9ac506a4c6c916e33d295717747abfc40a4125
kmanji <inkspand@gmail.com>
2014-02-17 19:55:20 -0500

    Updated profile and added scoped status updates

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -18,4 +18,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
 		assert_response :success
 	end
 
+	test 'that a profile page works ' do
+		get '/kmanji'
+		assert_response :success
+	end
 end
